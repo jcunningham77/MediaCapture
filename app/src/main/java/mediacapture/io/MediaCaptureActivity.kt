0bd2fe356785877f8d1cd780ccac19eb1c461795
Jeff Cunningham <jcunningham77@gmail.com>
2023-10-23 16:50:14 -0400

    rename

--- app/src/main/java/mediacapture/io/MediaCaptureActivity.kt
+++ app/src/main/java/mediacapture/io/MediaCaptureActivity.kt
@@ -3,16 +3,16 @@ package mediacapture.io
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.painterResource
 import mediacapture.io.ui.theme.MediaCaptureTheme
 
-class MainActivity : ComponentActivity() {
+class MediaCaptureActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    FlipCameraButton()
                 }
             }
         }
@@ -30,17 +30,12 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun FlipCameraButton() {
+    Image(
+        painterResource(id = R.drawable.baseline_flip_camera_android_24),
+        contentDescription = null
     )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MediaCaptureTheme {
-        Greeting("Android")
-    }
-}
+
+
