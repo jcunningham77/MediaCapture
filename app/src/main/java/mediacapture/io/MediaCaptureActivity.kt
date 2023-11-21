093a0b19af03dae7355110d4a25d98eb05e276ed
Jeff Cunningham <jcunningham77@gmail.com>
2023-11-21 11:27:56 -0500

    Constraint Layout

--- app/src/main/java/mediacapture/io/MediaCaptureActivity.kt
+++ app/src/main/java/mediacapture/io/MediaCaptureActivity.kt
@@ -2,23 +2,21 @@ package mediacapture.io
 
 import android.content.Context
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.camera.view.PreviewView
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import mediacapture.io.ui.theme.MediaCaptureTheme
 
 class MediaCaptureActivity : ComponentActivity() {
@@ -28,24 +26,46 @@ class MediaCaptureActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MediaCaptureTheme {
-                // A surface container using the 'background' color from the theme
-                Column {
-                    PreviewSurface(context = this@MediaCaptureActivity.baseContext)
-                    BottomNavigation()
-                }
+                ConstraintLayoutContent()
 
             }
         }
     }
 }
 
-//@Composable
-//fun PreviewSurface(context: Context) {
-//
-//    Row(Modifier.height(500.dp)) {
-//        PreviewView(context)
-//    }
-//}
+@Composable
+fun ConstraintLayoutContent() {
+    ConstraintLayout(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp)
+    ) {
+        val (flipCameraButton, recordButton) = createRefs()
+
+        FlipCameraButton(
+            Modifier
+                .constrainAs(flipCameraButton) {
+                    top.linkTo(recordButton.top)
+                    bottom.linkTo(recordButton.bottom)
+                    start.linkTo(parent.start)
+
+                }
+                .size(50.dp, 50.dp))
+
+        RecordButton(
+            Modifier
+                .constrainAs(recordButton) {
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+
+                }
+                .size(100.dp, 100.dp))
+
+    }
+
+}
+
 
 @Composable
 fun PreviewSurface(context: Context) {
@@ -55,28 +75,22 @@ fun PreviewSurface(context: Context) {
     }
 }
 
-@Composable
-fun BottomNavigation() {
-
-        Row(modifier = Modifier.height(100.dp)) {
-            FlipCameraButton()
-            RecordButton()
-        }
-}
 
 @Composable
-fun FlipCameraButton() {
+fun FlipCameraButton(modifier: Modifier) {
     Image(
         painterResource(id = R.drawable.baseline_flip_camera_android_24),
-        contentDescription = null
+        contentDescription = null,
+        modifier = modifier
     )
 }
 
 @Composable
-fun RecordButton() {
+fun RecordButton(modifier: Modifier) {
     Image(
         painterResource(id = R.drawable.baseline_fiber_manual_record_24),
-        contentDescription = null
+        contentDescription = null,
+        modifier = modifier,
     )
 }
 
