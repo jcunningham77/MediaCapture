7b7a31b775772e2c9c33c82b660bd0738935c42f
Jeff Cunningham <jcunningham77@gmail.com>
2023-11-21 09:32:28 -0500

    WIP layout

--- app/src/main/java/mediacapture/io/MediaCaptureActivity.kt
+++ app/src/main/java/mediacapture/io/MediaCaptureActivity.kt
@@ -4,12 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import mediacapture.io.ui.theme.MediaCaptureTheme
 
 class MediaCaptureActivity : ComponentActivity() {
@@ -18,21 +19,33 @@ class MediaCaptureActivity : ComponentActivity() {
         setContent {
             MediaCaptureTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    FlipCameraButton()
-                }
+                BottomNavigation()
             }
         }
     }
 }
 
 @Composable
+fun BottomNavigation() {
+
+        Row(modifier = Modifier.height(100.dp)) {
+            FlipCameraButton()
+            RecordButton()
+        }
+}
+
+@Composable
 fun FlipCameraButton() {
     Image(
         painterResource(id = R.drawable.baseline_flip_camera_android_24),
+        contentDescription = null
+    )
+}
+
+@Composable
+fun RecordButton() {
+    Image(
+        painterResource(id = R.drawable.baseline_fiber_manual_record_24),
         contentDescription = null
     )
 }
