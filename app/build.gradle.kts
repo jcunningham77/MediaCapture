c1a7cd248815475533d108f1dcd24058b6f155f9
Jeff Cunningham <jcunningham77@gmail.com>
2024-03-10 09:53:05 -0400

    app dependency cleanup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,14 +54,16 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-reactivestreams-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.0")
+
 
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
     implementation("androidx.constraintlayout:constraintlayout-compose")
+    implementation("androidx.activity:activity-compose")
+
+    implementation("androidx.compose.material3:material3")
 
     implementation(project(mapOf("path" to ":media-capture")))
 
