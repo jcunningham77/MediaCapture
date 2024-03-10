de5f905bda0cd9986b3f7bbd1286fc2ef08bd8c3
Jeff Cunningham <jcunningham77@gmail.com>
2024-03-10 10:06:43 -0400

    lib deps

--- media-capture/build.gradle.kts
+++ media-capture/build.gradle.kts
@@ -42,23 +42,28 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-reactivestreams-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.0")
+
+
     implementation(platform("androidx.compose:compose-bom:2024.02.02"))
-    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.activity:activity-compose")
+
+    // Not managed by compose-bom
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
     implementation("androidx.compose.material3:material3")
-    implementation("io.reactivex.rxjava2:rxandroid:2.1.1")
+
+    // camera X
     implementation("androidx.camera:camera-core:1.3.0-alpha07")
     implementation("androidx.camera:camera-camera2:1.3.0-alpha07")
     implementation("androidx.camera:camera-lifecycle:1.3.0-alpha07")
     implementation("androidx.camera:camera-video:1.3.0-alpha07")
     implementation("androidx.camera:camera-view:1.3.0-alpha07")
     implementation("androidx.camera:camera-extensions:1.3.0-alpha07")
+
+    // RX
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-reactivestreams-ktx:2.6.2")
+    implementation("io.reactivex.rxjava2:rxandroid:2.1.1")
 }
