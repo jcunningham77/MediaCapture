88a1a0313e91c559802619df5134fdee739d9001
Jeff Cunningham <jcunningham77@gmail.com>
2024-05-01 14:44:13 -0400

    formatting

--- media-capture/src/test/java/mediacapture/io/MediaCaptureViewModelTest.kt
+++ media-capture/src/test/java/mediacapture/io/MediaCaptureViewModelTest.kt
@@ -1,19 +1,17 @@
 package mediacapture.io
 
-import android.app.Application
-import mediacapture.io.livedata.observe
 import org.junit.Test
-import org.mockito.Mockito
 import org.mockito.kotlin.mock
 
 class MediaCaptureViewModelTest {
 
     @Test
-    fun `Recording State emitted`(){
+    fun `Recording State emitted`() {
         val retrieveRecentMediaUseCase: RetrieveRecentMediaUseCase = mock()
         val processCameraProviderUseCase: ProcessCameraProviderUseCase = mock()
 
-        val viewModel = MediaCaptureViewModel(retrieveRecentMediaUseCase, processCameraProviderUseCase)
+        val viewModel =
+            MediaCaptureViewModel(retrieveRecentMediaUseCase, processCameraProviderUseCase)
 
         val testObserver = viewModel.viewState.test()
 
