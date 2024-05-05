bf4f5dfef73e6a19e64e651d26cd498fa0381f46
Jeff Cunningham <jcunningham77@gmail.com>
2024-05-05 10:25:10 -0400

    Github Actions: Unit Test

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,3 +19,20 @@ dependencyResolutionManagement {
 rootProject.name = "MediaCapture"
 include(":app")
 include(":media-capture")
+
+plugins {
+    id("com.gradle.enterprise") version("3.16.2")
+}
+
+gradleEnterprise {
+    if (System.getenv("CI") != null) {
+        println("This build is running on Github")
+//        buildScan {
+//            publishAlways()
+//            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+//            termsOfServiceAgree = "yes"
+//        }
+    } else {
+        println("This build is running locally")
+    }
+}
