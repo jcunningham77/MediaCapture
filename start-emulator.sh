ee759281a018cc0e59deeed09054b28d64ff2b35
Jeff Cunningham <jcunningham77@gmail.com>
2024-07-18 14:21:02 -0400

    create AVD

--- start-emulator.sh
+++ start-emulator.sh
@@ -9,9 +9,21 @@ export PATH=$PATH:$ANDROID_HOME/emulator
 echo "PATH = $PATH"
 
 # Start the Android emulator
+echo "list of AVDs: (1)"
 emulator -list-avds
 
+# List available system images
+echo "list of system images:"
+sdkmanager --list | grep system-images
+
+# Example
+avdmanager create avd -n "pixel8-Android35" -k "system-images;android-35;google_apis_playstore;arm64-v8a" -d "pixel"
+
+echo "list of AVDs: (2)"
+emulator -list-avds
+
+
 # Wait for the emulator to fully boot
-adb wait-for-device
-adb shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done;'
-adb shell input keyevent 82
+#adb wait-for-device
+#adb shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done;'
+#adb shell input keyevent 82
